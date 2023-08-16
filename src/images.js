0b700b1226afa6699f3a70a4784ab4573fc352c0
meck <meckgalen@gmail.com>
2023-08-16 22:41:01 +0300

    Images added

---
+++ src/images.js
@@ -0,0 +1,4 @@
+import Tomato from './img/tomato_soup.jpg'
+
+const imgTomato = new Image()
+imgTomato.src = Tomato
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/reset.css'
 import './styles/main.css'
 import './tab.js'
+import './images'
 
 
 
